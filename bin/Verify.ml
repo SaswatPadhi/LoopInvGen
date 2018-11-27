b724575a30ebd4c92fad03f19cc65bea4d854413
Saswat Padhi <padhi@cs.ucla.edu>
2018-11-27 09:03:42 +0000

    Weak type checking in verification script, for older solvers

--- bin/Verify.ml
+++ bin/Verify.ml
@@ -27,7 +27,7 @@ let check_invariant ~(zpath : string) ~(sygus : SyGuS.t) (inv : string) : result
                                  ~f:(fun i v -> if v = None then None
                                                 else Some [| "pre" ; "trans" ; "post" |].(i)))))
 
-let read_inv_from_chan in_chan ~(sygus : SyGuS.t) : Sexplib.Sexp.t =
+let read_inv_from_chan weak_types in_chan ~(sygus : SyGuS.t) : Sexplib.Sexp.t =
   let open Sexplib.Sexp in
   let sexps = input_rev_sexps in_chan
   in match sexps with
@@ -39,9 +39,33 @@ let read_inv_from_chan in_chan ~(sygus : SyGuS.t) : Sexplib.Sexp.t =
              | Atom s -> Atom (if String.equal s old_v then new_v else s)
              | List s -> List (List.map ~f:internal_helper s)
             in internal_helper expr
-       in List.foldi vars ~init:body
-            ~f:(fun [@warning "-8"] i cur_body (List [ (Atom v) ; _ ])
-                -> helper cur_body v (fst (List.nth_exn sygus.inv_func.args i)))
+          in let bstr = function "0" -> "false" | "1" -> "true" | s -> s
+          in let rec helper_2 = function
+               | List [ (Atom "ite") ; (Atom b) ; x ; y ]
+                 -> List [ (Atom "ite") ; (Atom (bstr b)) ; (helper_2 x) ; (helper_2 y) ]
+               | List [ (Atom "=>") ; (Atom x) ; (Atom y) ]
+                 -> List [ (Atom "=>") ; (Atom (bstr x)) ; (Atom (bstr y)) ]
+               | List [ (Atom "=>") ; (Atom x) ; (List _ as y) ]
+                 -> List [ (Atom "=>") ; (Atom (bstr x)) ; (helper_2 y) ]
+               | List [ (Atom "=>") ; (List _ as y) ; (Atom x) ]
+                 -> List [ (Atom "=>") ; (helper_2 y) ; (Atom (bstr x)) ]
+               | List [ (Atom "or") ; (Atom x) ; (Atom y) ]
+                 -> List [ (Atom "or") ; (Atom (bstr x)) ; (Atom (bstr y)) ]
+               | List [ (Atom "or") ; (Atom x) ; (List _ as y) ]
+               | List [ (Atom "or") ; (List _ as y) ; (Atom x) ]
+                 -> List [ (Atom "or") ; (Atom (bstr x)) ; (helper_2 y) ]
+               | List [ (Atom "and") ; (Atom x) ; (Atom y) ]
+                 -> List [ (Atom "and") ; (Atom (bstr x)) ; (Atom (bstr y)) ]
+               | List [ (Atom "and") ; (Atom x) ; (List _ as y) ]
+               | List [ (Atom "and") ; (List _ as y) ; (Atom x) ]
+                 -> List [ (Atom "and") ; (Atom (bstr x)) ; (helper_2 y) ]
+               | List [ (Atom "not") ; (Atom x) ]
+                 -> List [ (Atom "not") ; (Atom (bstr x)) ]
+               | List sexps -> List (List.map ~f:helper_2 sexps)
+               | sexp -> sexp
+          in List.foldi vars ~init:(if weak_types then helper_2 body else body)
+               ~f:(fun [@warning "-8"] i cur_body (List [ (Atom v) ; _ ])
+                   -> helper cur_body v (fst (List.nth_exn sygus.inv_func.args i)))
      | _ -> raise (Exceptions.Parse_Exn
                      "Multiple Sexps detected, expecting a single invariant.")
 
@@ -59,10 +83,10 @@ let exit_code_of_result res =
   | NO_SOLUTION_PASS -> 2
   | NO_SOLUTION_FAIL -> 3
 
-let main zpath filename logfile invfile () =
+let main zpath filename logfile weak_types invfile () =
   Log.enable ~msg:"VERIFY" logfile ;
   let sygus = SyGuS.parse (get_in_channel filename) in
-  let inv = read_inv_from_chan (get_in_channel invfile) ~sygus in
+  let inv = read_inv_from_chan weak_types (get_in_channel invfile) ~sygus in
   let res =
     try check_invariant ~zpath ~sygus (Sexplib.Sexp.to_string_hum inv)
     with _ -> FAIL [ "<parse>" ]
@@ -78,6 +102,8 @@ let spec =
        ~doc:"FILENAME input file containing the SyGuS-INV problem"
     +> flag "-l" (optional string)
        ~doc:"FILENAME output file for logs, defaults to null"
+    +> flag "-t" no_arg
+       ~doc:"Weak type checking. Allow: 0 for false, 1 for true in invariant"
     +> anon (maybe_with_default "-" ("filename" %: file))
   )
 
