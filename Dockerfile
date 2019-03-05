SaswatPadhi/LoopInvGen

commit ca9393f4d7ab75ffc28a2e34176ba034bef93b05

Error checking and minor fixes in Bash  scripts

Dockerfile
@@ -12,11 +12,19 @@ ENV Z3_VERSION    4.8.4
 ENV HOME /home/opam
 
 
-ARG DEBIAN_FRONTEND=noninteractive
+ENV DEBIAN_FRONTEND=noninteractive
 RUN apt update && \
     apt upgrade -yq && \
-    apt install -yq aspcud binutils cmake curl g++ git libgmp-dev libgomp1 libomp5 \
-                    libomp-dev libx11-dev m4 make patch python2.7 sudo time tzdata unzip
+    apt install -yq aspcud \
+                    binutils \
+                    cmake curl \
+                    g++ git \
+                    libgmp-dev libgomp1 libomp5 libomp-dev libx11-dev \
+                    m4 make \
+                    patch python2.7 \
+                    sudo \
+                    time tzdata \
+                    unzip
 RUN apt autoremove -y --purge
 
 

loopinvgen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if (( ${BASH_VERSION%%.*} < 4 )); then echo "ERROR: [bash] version 4.0+ required!" ; exit -1 ; fi
+set -Euo pipefail
+
+if (( ${BASH_VERSION%%.*} < 4 )); then echo "ERROR: [bash] version >= 4.0 required!" ; exit -1 ; fi
 
 EXIT_CODE_BUILD_ERROR=-3
 EXIT_CODE_USAGE_ERROR=-2
@@ -42,18 +44,18 @@ MIN_RECORD_STATES_PER_FORK=63
 
 EXPRESSIVENESS_LEVEL=4
 
-PROCESS_LOG=""
-RECORD_LOG=""
-INFER_LOG=""
-VERIFY_LOG=""
-
 PROCESS_ARGS=""
 RECORD_ARGS=""
 INFER_ARGS=""
 VERIFY_ARGS=""
 
 declare -A DO_LOG
+DO_LOG[process]="no"
+DO_LOG[record]="no"
+DO_LOG[infer]="no"
+DO_LOG[verify]="no"
 
+STATS_ARG=""
 DO_CLEAN="no"
 DO_VERIFY="no"
 
@@ -88,68 +90,73 @@ Arguments to Internal Programs (@ `dirname $RECORD`):
 " >&2 ; exit $EXIT_CODE_USAGE_ERROR
 }
 
-OPTS=`getopt -n 'parse-options' -o :P:R:I:V:cvl:L:p:s:S:t:z: --long Process-args:,Record-args:,Infer-args:,Verify-args:,clean-intermediates,verify,log:,expressiveness-level:,intermediates-dir:,states-to-record:,stats-path:,infer-timeout:,z3-path: -- "$@"`
-if [ $? != 0 ]; then usage ; fi
+for opt in "$@"; do
+  shift
+  case "$opt" in
+    "--Process-args")         set -- "$@" "-P" ;;
+    "--Record-args")          set -- "$@" "-R" ;;
+    "--Infer-args")           set -- "$@" "-I" ;;
+    "--Verify-args")          set -- "$@" "-V" ;;
 
-eval set -- "$OPTS"
+    "--clean-intermediates")  set -- "$@" "-c" ;;
+    "--verify")               set -- "$@" "-v" ;;
 
-while true ; do
-  case "$1" in
-    -P | --Process-args )
-         PROCESS_ARGS="$2"
-         shift ; shift ;;
-    -R | --Record-args )
-         RECORD_ARGS="$2"
-         shift ; shift ;;
-    -I | --Infer-args )
-         INFER_ARGS="$2"
-         shift ; shift ;;
-    -V | --Verify-args )
-         VERIFY_ARGS="$2"
-         shift ; shift ;;
+    "--log")                  set -- "$@" "-l" ;;
+    "--expressiveness-level") set -- "$@" "-L" ;;
+    "--intermediates-path")   set -- "$@" "-p" ;;
+    "--states-to-record")     set -- "$@" "-s" ;;
+    "--stats-path")           set -- "$@" "-S" ;;
+    "--infer-timeout")        set -- "$@" "-t" ;;
+    "--z3-path")              set -- "$@" "-z" ;;
 
-    -c | --clean-intermediates )
-         DO_CLEAN="yes" ; shift ;;
-    -v | --verify )
-         DO_VERIFY="yes" ; shift ;;
-
-    -l | --log )
-         for LOG_SRC in `echo "$2" | tr ',' '\n' | sort -u | tr '\n' ' '` ; do
-           case "$LOG_SRC" in
-             process | record | infer | verify ) DO_LOG[$LOG_SRC]="yes" ;;
-             * ) usage "Unknown source [$LOG_SRC] for logging."
-           esac
-         done
-         shift ; shift ;;
-    -L | --expressiveness-level )
-         [ "$2" -ge "1" ] && [ "$2" -le "6" ] \
-           || usage "The expressiveness level (= $2) must be between 1 and 6 (both inclusive)."
-         EXPRESSIVENESS_LEVEL="$2"
-         shift ; shift ;;
-    -p | --intermediates-dir )
-         INTERMEDIATES_DIR="$2"
-         shift ; shift ;;
-    -s | --states-to-record )
-         [ "$2" -gt "$MIN_RECORD_STATES_PER_FORK" ] \
-           || usage "The number of states to record (= $2) must be > $MIN_RECORD_STATES_PER_FORK."
-         STATES="$2"
-         shift ; shift ;;
-    -S | --stats-path )
-         STATS_ARG="-t $2"
-         shift ; shift ;;
-    -t | --infer-timeout )
-         [ "$2" -gt "$MIN_INFER_TIMEOUT" ] \
-           || usage "The inference timeout (= $2) must be > $MIN_INFER_TIMEOUT."
-         INFER_TIMEOUT="$2"
-         shift ; shift ;;
-    -z | --z3-path )
-         [ -f "$2" ] || usage "Z3 [$2] not found."
-         Z3_PATH="$2"
-         shift ; shift ;;
-    -- ) shift; break ;;
-    * ) break ;;
+    "--")                     set -- "$@" "--" ;;
+    "--"*)                    usage "Unrecognized option: $opt." ;;
+    *)                        set -- "$@" "$opt"
   esac
 done
+
+OPTIND=1
+while getopts ':P:R:I:V:cvl:L:p:s:S:t:z:' OPTION ; do
+  case "$OPTION" in
+    "P" ) PROCESS_ARGS="$OPTARG" ;;
+    "R" ) RECORD_ARGS="$OPTARG" ;;
+    "I" ) INFER_ARGS="$OPTARG" ;;
+    "V" ) VERIFY_ARGS="$OPTARG" ;;
+
+    "c" ) DO_CLEAN="yes" ;;
+    "v" ) DO_VERIFY="yes" ;;
+
+    "l" ) for LOG_SRC in `echo "$OPTARG" | tr ',' '\n' | sort -u | tr '\n' ' '` ; do
+            case "$LOG_SRC" in
+              process | record | infer | verify ) DO_LOG[$LOG_SRC]="yes" ;;
+              * ) usage "Unknown source [$LOG_SRC] for logging."
+            esac
+          done
+          ;;
+    "L" ) [ "$OPTARG" -ge "1" ] && [ "$OPTARG" -le "6" ] \
+            || usage "The expressiveness level (= $OPTARG) must be between 1 and 6 (both inclusive)."
+          EXPRESSIVENESS_LEVEL="$OPTARG"
+          ;;
+    "p" ) INTERMEDIATES_DIR="$OPTARG"
+          ;;
+    "s" ) [ "$OPTARG" -gt "$MIN_RECORD_STATES_PER_FORK" ] \
+            || usage "The number of states to record (= $OPTARG) must be > $MIN_RECORD_STATES_PER_FORK."
+          STATES="$OPTARG"
+          ;;
+    "S" ) rm -rf $OPTARG
+          STATS_ARG="-t $OPTARG"
+          ;;
+    "t" ) [ "$OPTARG" -gt "$MIN_INFER_TIMEOUT" ] \
+            || usage "The inference timeout (= $OPTARG) must be > $MIN_INFER_TIMEOUT."
+          INFER_TIMEOUT="$OPTARG"
+          ;;
+    "z" ) [ -f "$OPTARG" ] || usage "Z3 [$OPTARG] not found."
+          Z3_PATH="$OPTARG"
+          ;;
+      * ) usage "Unrecognized option: -$OPTARG." ;;
+  esac
+done
+shift $(($OPTIND -1))
 
 [ -d "$INTERMEDIATES_DIR" ] || mkdir -p "$INTERMEDIATES_DIR"
 [ -d "$INTERMEDIATES_DIR" ] \
@@ -181,7 +188,7 @@ INFER_TIMEOUT="${INFER_TIMEOUT}s"
 [ -z "${DO_LOG[infer]}" ] || DO_LOG[infer]="-l $TESTCASE_ALL_LOG"
 [ -z "${DO_LOG[verify]}" ] || DO_LOG[verify]="-l $TESTCASE_ALL_LOG"
 
-rm -rf "$TESTCASE_REC_STATES"? $TESTCASE_ALL_STATES $TESTCASE_STATISTICS &
+rm -rf "$TESTCASE_REC_STATES"? $TESTCASE_ALL_STATES &
 echo -en '' > "$TESTCASE_INVARIANT" &
 echo -en '' > "$TESTCASE_ALL_LOG"
 
@@ -205,7 +212,7 @@ wait
 
 grep -hv "^[[:space:]]*$" "$TESTCASE_REC_STATES"? | sort -u > "$TESTCASE_ALL_STATES"
 
-[ -z "${DO_LOG[record]}" ] || cat "$TESTCASE_REC_LOG"? >> "$TESTCASE_ALL_LOG"
+[ -z "${DO_LOG[record]}" ] || cat "$TESTCASE_REC_LOG"? >> "$TESTCASE_ALL_LOG" 2> /dev/null || true
 
 
 show_status "(inferring)"

scripts/build_all.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
+set -Eeuo pipefail
+
 if (( ${BASH_VERSION%%.*} < 4 )); then echo "ERROR: [bash] version 4.0+ required!" ; exit -1 ; fi
 
 ROOT="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)/.."
 
 usage() {
-  echo "
+  if [ -n "$1" ]; then echo -e "\nERROR: $1" >&2 ; fi
+  echo -en "
 Usage: $0 [flags]
 
 Flags:
     [--debug, -D]             Create a debug build (minimal optimization and allows logging)
-    [--star-exec, -S]         Generate package for running on StarExec
+    [--starexec, -S]         Generate package for running on StarExec
     [--with-logging, -L]      Enable logging within the LoopInvGen binaries
 
 Configuration:
     [--jobs, -j <num>]        Use <num> jobs for building LoopInvGen (and z3)
     [--build-z3, -z <path>]   Also create a statically-linked build of z3
-" 1>&2 ;
-  exit 1 ;
+" 1>&2 ; exit 1
 }
-
-OPTS=`getopt -n 'parse-options' -o :DSLj:z: --long debug,star-exec,with-logging,jobs:,build-z3: -- "$@"`
-if [ $? != 0 ] ; then usage ; fi
-
-eval set -- "$OPTS"
 
 JOBS="`cat /proc/cpuinfo | grep processor | wc -l`"
 
@@ -34,27 +31,33 @@ MAKE_DEBUG=""
 
 STAREXEC_ARCHIVE_NAME="LoopInvGen_SyGuS_INV.tgz"
 
-while true ; do
-  case "$1" in
-    -D | --debug )
-         MAKE_DEBUG="YES"
-         shift ;;
-    -S | --star-exec )
-         MAKE_STAREXEC="YES" ;
-         shift ;;
-    -L | --with-logging )
-         WITH_LOGGING="YES" ;
-         shift ;;
-    -j | --jobs )
-         JOBS="$2" ;
-         shift ; shift ;;
-    -z | --build-z3 )
-         MAKE_Z3_AT="`realpath $2`" ;
-         shift ; shift ;;
-    -- ) shift; break ;;
-    * ) break ;;
+for opt in "$@"; do
+  shift
+  case "$opt" in
+    "--debug")        set -- "$@" "-D" ;;
+    "--starexec")     set -- "$@" "-S" ;;
+    "--with-logging") set -- "$@" "-L" ;;
+    "--jobs")         set -- "$@" "-j" ;;
+    "--build-z3")     set -- "$@" "-z" ;;
+
+    "--")             set -- "$@" "--" ;;
+    "--"*)            usage "Unrecognized option: $opt." ;;
+    *)                set -- "$@" "$opt"
   esac
 done
+
+OPTIND=1
+while getopts ':DSLj:z:' OPTION ; do
+  case "$OPTION" in
+    "D" ) MAKE_DEBUG="YES" ;;
+    "S" ) MAKE_STAREXEC="YES" ;;
+    "L" ) WITH_LOGGING="YES" ;;
+    "j" ) JOBS="$OPTARG" ;;
+    "z" ) MAKE_Z3_AT="`realpath "$OPTARG"`" ;;
+      * ) usage "Unrecognized option: -$OPTARG." ;;
+  esac
+done
+shift $(($OPTIND -1))
 
 if [ -n "$MAKE_Z3_AT" ] ; then
   Z3_BUILD_DIR="build_for_lig"
@@ -89,15 +92,15 @@ fi
 
 if [ -z "$MAKE_STAREXEC" ] ; then exit 0 ; fi
 
-rm -rf starexec && mkdir -p starexec/bin/_bin
+rm -rf _starexec && mkdir -p _starexec/bin/_bin
 
-cp -rL _dep starexec/bin
-cp -L _build/install/default/bin/* starexec/bin/_bin
-rm -rf starexec/bin/_bin/lig-verify starexec/bin/_bin/lig-score
+cp -rL _dep _starexec/bin
+cp -L _build/install/default/bin/* _starexec/bin/_bin
+rm -rf _starexec/bin/_bin/lig-verify _starexec/bin/_bin/lig-score
 
-STAREXEC_ZERO_CONFIG_FILE="starexec/bin/starexec_run_zero"
-STAREXEC_DEFAULT_CONFIG_FILE="starexec/bin/starexec_run_default"
-STAREXEC_DEBUG_CONFIG_FILE="starexec/bin/starexec_run_IGNORE-debug"
+STAREXEC_ZERO_CONFIG_FILE="_starexec/bin/starexec_run_zero"
+STAREXEC_DEFAULT_CONFIG_FILE="_starexec/bin/starexec_run_default"
+STAREXEC_DEBUG_CONFIG_FILE="_starexec/bin/starexec_run_IGNORE-debug"
 
 cat > "$STAREXEC_DEFAULT_CONFIG_FILE" << "EOF"
 #!/bin/bash
@@ -154,15 +157,15 @@ _bin/lig-infer -h
 EOF
 chmod +x "$STAREXEC_DEBUG_CONFIG_FILE"
 
-cat <<EOF > starexec/starexec_description.txt
+cat <<EOF > _starexec/starexec_description.txt
 A loop invariant inference tool built using PIE: precondition inference engine.
 
 https://github.com/SaswatPadhi/LoopInvGen
 EOF
 
-chmod -R 777 starexec/bin
+chmod -R 777 _starexec/bin
 
-echo -e "\nPreparing starexec package (starexec/):"
-cd starexec
-tar cvzf "../$STAREXEC_ARCHIVE_NAME" ./*
+echo -e "\nPreparing package for StarExec (from _starexec/):"
+cd _starexec
+tar cvzf "../$STAREXEC_ARCHIVE_NAME" *
 echo -e "\nPackage saved to $STAREXEC_ARCHIVE_NAME"

.gitignore
@@ -1,7 +1,3 @@
-config.h
-*.tgz
-starexec*
-
 # - - -
 # OCaml
 # - - -
@@ -17,9 +13,6 @@ starexec*
 *.cmxs
 *.cmxa
 
-# ocamlbuild working directory
-_build/
-
 # ocamlbuild targets
 *.byte
 *.native
@@ -29,16 +22,13 @@ _build/
 *.install
 
 # External
-_dep
-_log*
-_comp
+/_*
 
 # Temporaries
 ocamlprof.dump
 gmon.out
-_tags
-
-
+config.h
+/*.tgz
 
 # - - -
 # Editor/IDE

.travis.yml
@@ -8,21 +8,7 @@ branches:
 services:
   - docker
 
-before_install:
-  - docker run --name opam -td ocaml/opam2
-  - docker cp `pwd` opam:/travis
-  - docker exec opam bash -c "sudo chown -R opam:opam /travis"
-
-install: docker exec opam bash -c "sudo apt -yqq install m4"
-
-script: |
-  docker exec -t \
-              -e OCAML_VERSION="$OCAML_VERSION" \
-              -e PROFILE="$PROFILE" \
-              -e MIN_REQS_ONLY="$MIN_REQS_ONLY" \
-              opam bash -c "cd /travis && bash -ex .travis.sh"
-
-after_script: docker stop opam
+install: bash .travis/install.sh
 
 env:
   - OCAML_VERSION=4.05.0          PROFILE="optimize"  MIN_REQS_ONLY="true"
@@ -39,4 +25,5 @@ env:
   - OCAML_VERSION=4.06.1+flambda  PROFILE="debug"
   - OCAML_VERSION=4.07.1          PROFILE="debug"
   - OCAML_VERSION=4.07.1+flambda  PROFILE="debug"
-  
+
+script: bash .travis/script.sh

.travis/check.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
+[[ -z $CHANGED_FILES ]] && exit 1
+
+for CHANGED_FILE in $CHANGED_FILES; do
+  if ! [[ $CHANGED_FILE =~ ^docs/ || $CHANGED_FILE =~ .md$ ]]; then
+    exit 1
+  fi
+done

.travis/install.sh
@@ -1,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+ROOT="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+CHANGED="`bash $ROOT/check.sh ; echo $?`"
+if [ "$CHANGED" -eq "0" ]; then
+    echo "Skipping `docker` installation since no source files have been modified."
+else
+    docker run --name ocaml -td ocaml/opam2
+    docker exec ocaml bash -c "sudo apt -yqq install m4"
+    docker cp "$ROOT/.." ocaml:/LoopInvGen
+    docker exec ocaml bash -c "sudo chown -R opam:opam /LoopInvGen"
+fi

commit acd023fec4dc03591b8b3652b9a3f062930193ed

Fixed a logging issue introduced in ca9393f4

.gitattributes
@@ -1,2 +1,3 @@
-benchmarks/* linguist-vendored
-docs/* linguist-vendored
+benchmarks/*    linguist-vendored
+docs/*          linguist-vendored
+.travis.*       linguist-vendored

Dockerfile
@@ -21,7 +21,7 @@ RUN apt update && \
                     g++ git \
                     libgmp-dev libgomp1 libomp5 libomp-dev libx11-dev \
                     m4 make \
-                    patch python2.7 \
+                    patch python3 \
                     sudo \
                     time tzdata \
                     unzip

loopinvgen.sh
@@ -50,10 +50,10 @@ INFER_ARGS=""
 VERIFY_ARGS=""
 
 declare -A DO_LOG
-DO_LOG[process]="no"
-DO_LOG[record]="no"
-DO_LOG[infer]="no"
-DO_LOG[verify]="no"
+DO_LOG[process]=""
+DO_LOG[record]=""
+DO_LOG[infer]=""
+DO_LOG[verify]=""
 
 STATS_ARG=""
 DO_CLEAN="no"
@@ -202,6 +202,7 @@ $PROCESS -o "$TESTCASE_PROCESSED" "$TESTCASE" ${DO_LOG[process]} $PROCESS_ARGS >
 wait
 show_status "(recording)"
 
+LOG_PARAM=""
 for i in `seq 1 $RECORD_FORKS` ; do
   [ -z "${DO_LOG[record]}" ] || LOG_PARAM="${DO_LOG[record]}$i"
   (timeout $RECORD_TIMEOUT \

scripts/build_all.sh
@@ -66,8 +66,8 @@ if [ -n "$MAKE_Z3_AT" ] ; then
   rm -rf "$Z3_BUILD_DIR"
   mkdir -p "$Z3_BUILD_DIR"
 
-  python2.7 scripts/mk_make.py --staticbin --staticlib \
-                               --build "$Z3_BUILD_DIR"
+  python3 scripts/mk_make.py --staticbin --staticlib \
+                             --build "$Z3_BUILD_DIR"
 
   cd "$Z3_BUILD_DIR"
   make -j "$JOBS"