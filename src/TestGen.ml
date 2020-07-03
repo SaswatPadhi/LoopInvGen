3a2d80d97eae0cdf04e1056a951341f36ffc3a6e
juliaofferman <59590610+juliaofferman@users.noreply.github.com>
2020-07-02 19:30:09 -0700

    Update TestGen.ml

--- src/TestGen.ml
+++ src/TestGen.ml
@@ -6,7 +6,7 @@ let rec for_type (t : Type.t) : Value.t Generator.t =
   match t with
   (* Full range of Int:
   | Type.INT -> Int.gen >>= fun i -> singleton (Value.Int i) *)
-  | Type.INT -> (Int.gen_incl (-4096) 4095) >>= fun i -> singleton (Value.Int i)
+  | Type.INT -> (Int.gen_incl (-4096) 4096) >>= fun i -> singleton (Value.Int i)
   | Type.BOOL -> bool >>= fun b -> singleton (Value.Bool b)
   | Type.CHAR -> char >>= fun c -> singleton (Value.Char c)
   | Type.STRING -> (Int.gen_incl 0 64)
