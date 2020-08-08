189fb5c4ba729464378a9741bc4473ee928def9c
Julia Offerman <59590610+juliaofferman@users.noreply.github.com>
2020-08-08 12:47:26 -0700

    Add additional tests

--- test/Test_RealComponents.ml
+++ test/Test_RealComponents.ml
@@ -31,10 +31,35 @@ let add () =
   let res = equal 0 (Core_kernel.Float.robustly_compare (value_of add_ret) 11.06)
    in Alcotest.(check bool) "identical" true res
 
+let add_zero () =
+  let rl = [Value.Real 993.54 ; Value.Real 0.] in 
+  let add_ret = add_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of add_ret) 993.54)
+   in Alcotest.(check bool) "identical" true res
+
+let add_neg () =
+  let rl = [Value.Real 3.54 ; Value.Real ~-.10.1] in 
+  let add_ret = add_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of add_ret) ~-.6.56)
+   in Alcotest.(check bool) "identical" true res
+
+
 let sub () =
   let rl = [Value.Real 7.52 ; Value.Real 3.54] in 
   let sub_ret = sub_eval rl in
   let res = equal 0 (Core_kernel.Float.robustly_compare (value_of sub_ret) 3.98)
+   in Alcotest.(check bool) "identical" true res
+
+let sub_zero () =
+  let rl = [Value.Real 7.543 ; Value.Real 0. ] in 
+  let sub_ret = sub_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of sub_ret) 7.543)
+   in Alcotest.(check bool) "identical" true res
+
+let sub_neg () =
+  let rl = [Value.Real 0. ; Value.Real 3.54] in 
+  let sub_ret = sub_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of sub_ret) ~-.3.54)
    in Alcotest.(check bool) "identical" true res
 
 let mult () =
@@ -43,10 +68,28 @@ let mult () =
   let res = equal 0 (Core_kernel.Float.robustly_compare (value_of mult_ret) 26.6208)
    in Alcotest.(check bool) "identical" true res
 
+let mult_zero () =
+  let rl = [Value.Real 3.54 ; Value.Real 0.] in 
+  let mult_ret = mult_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of mult_ret) 0.)
+   in Alcotest.(check bool) "identical" true res
+
+let mult_neg () =
+  let rl = [Value.Real 3.54 ; Value.Real ~-.5.42] in 
+  let mult_ret = mult_eval rl in
+  let res = equal 0 (Core_kernel.Float.robustly_compare (value_of mult_ret) ~-.19.1868)
+   in Alcotest.(check bool) "identical" true res
+
 let div () = 
   let rl = [Value.Real 3. ; Value.Real 2.] in 
   let div_ret = div_eval rl in
   let res = Value.equal div_ret (Value.Real 1.5)
+   in Alcotest.(check bool) "identical" true res
+
+let div_neg () = 
+  let rl = [Value.Real 3. ; Value.Real ~-.2.] in 
+  let div_ret = div_eval rl in
+  let res = Value.equal div_ret (Value.Real ~-.1.5)
    in Alcotest.(check bool) "identical" true res
 
 let eq () =
@@ -54,12 +97,35 @@ let eq () =
   let eq_ret = eq_eval rl in
   let res = Value.equal eq_ret (Value.Bool true)
    in Alcotest.(check bool) "identical" true res
-  
+
+let eq_not () =
+  let rl = [Value.Real 3.54 ; Value.Real 3.59] in 
+  let eq_ret = eq_eval rl in
+  let res = Value.equal eq_ret (Value.Bool false)
+   in Alcotest.(check bool) "identical" true res
+
+let eq_neg () =
+  let rl = [Value.Real 3.54 ; Value.Real ~-.3.54] in 
+  let eq_ret = eq_eval rl in
+  let res = Value.equal eq_ret (Value.Bool false)
+   in Alcotest.(check bool) "identical" true res
 
 let geq () =
   let rl = [Value.Real 7.52 ; Value.Real 3.54] in 
   let geq_ret = geq_eval rl in
   let res = Value.equal geq_ret (Value.Bool true)
+   in Alcotest.(check bool) "identical" true res
+
+let geq_eq () =
+  let rl = [Value.Real 7.52 ; Value.Real 7.52] in 
+  let geq_ret = geq_eval rl in
+  let res = Value.equal geq_ret (Value.Bool true)
+   in Alcotest.(check bool) "identical" true res
+
+let geq_not () =
+  let rl = [Value.Real 7.52 ; Value.Real 7.54] in 
+  let geq_ret = geq_eval rl in
+  let res = Value.equal geq_ret (Value.Bool false)
    in Alcotest.(check bool) "identical" true res
 
 let leq () = 
@@ -68,21 +134,55 @@ let leq () =
   let res = Value.equal leq_ret (Value.Bool true)
    in Alcotest.(check bool) "identical" true res
 
+let leq_eq () = 
+  let rl = [Value.Real 7.42 ; Value.Real 7.42] in 
+  let leq_ret = leq_eval rl in
+  let res = Value.equal leq_ret (Value.Bool true)
+   in Alcotest.(check bool) "identical" true res
+
+let leq_not () = 
+  let rl = [Value.Real ~-.3.54 ; Value.Real ~-.7.52] in 
+  let leq_ret = leq_eval rl in
+  let res = Value.equal leq_ret (Value.Bool false)
+   in Alcotest.(check bool) "identical" true res
+
 let ite () =
   let rl = [Value.Bool false ; Value.Real 3.54 ; Value.Real 7.52] in 
   let ite_ret = ite_eval rl in
   let res = Value.equal ite_ret (Value.Real 7.52)
    in Alcotest.(check bool) "identical" true res
 
+let ite_true () =
+  let rl = [Value.Bool true ; Value.Real 3.54 ; Value.Real 7.52] in 
+  let ite_ret = ite_eval rl in
+  let res = Value.equal ite_ret (Value.Real 3.54)
+   in Alcotest.(check bool) "identical" true res
+
+
+
 
 let all = [
   "add",              `Quick, add ;
+  "add_zero",         `Quick, add_zero ;
+  "add_neg",          `Quick, add_neg ;
   "sub",              `Quick, sub ;
-  "mult",              `Quick, mult ;
+  "sub_zero",         `Quick, sub_zero ;
+  "sub_neg",          `Quick, sub_neg ;
+  "mult",             `Quick, mult ;
+  "mult_zero",        `Quick, mult_zero ;
+  "mult_neg",         `Quick, mult_neg ;
   "div",              `Quick, div ;
+  "div_neg",           `Quick, div_neg ;
   "eq",               `Quick, eq ;
+  "eq_not",           `Quick, eq_not ;
+  "eq_neg",           `Quick, eq_neg ;
   "geq",              `Quick, geq ;
+  "geq_eq",           `Quick, geq_eq ;
+  "geq_not",          `Quick, geq_not ;
   "leq",              `Quick, leq ;
+  "leq_eq",           `Quick, leq_eq ;
+  "leq_not",          `Quick, leq_not ;
   "ite",              `Quick, ite ;
+  "ite_true",         `Quick, ite_true ;
 
 ]
